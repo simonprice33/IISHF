d037de3ca49aaca9cdbd56a8f020599938a6aab0
SJP002\Simon <simon.price@freelygive.org.uk>
2026-02-05 20:32:56 +0000

    starting on home page

--- iishf.web/src/app/layout.tsx
+++ iishf.web/src/app/layout.tsx
@@ -1,10 +1,17 @@
 import "./globals.css";
 import { NavBarContainer } from "@/components/Navbar/NavBarContainer";
 import { Footer } from "@/components/Footer/Footer";
+import localFont from "next/font/local";
+
+const raleway = localFont({
+  src: "../fonts/Raleway-VariableFont_wght.ttf",
+  variable: "--font-raleway",
+  weight: "100 900",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={raleway.variable}>
       <body>
         <NavBarContainer />
         {children}
---
+++ iishf.web/src/components/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { Hero } from "../Hero/Hero";
+
+export function Home() {
+  return (
+    <main>
+      <Hero />
+      {/* Additional sections will go here */}
+    </main>
+  );
+}
