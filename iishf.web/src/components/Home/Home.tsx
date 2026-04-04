cb7f0aa55f5b30c984d85851ee0fa745631bfd5a
Simon Price <sprice@iishf.com>
2026-04-04 12:21:36 +0100

    wip

--- iishf.web/src/components/Home/Home.tsx
+++ iishf.web/src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { Hero } from "../Hero/Hero";
-import { LiveStreamContainer } from "../Livestream/LivestreamContainer";
+import { LiveStreamContainer } from "../Livestream/LiveStreamContainer";
 import { LatestNewsContainer } from "../News/LatestNewsContainer";
 import { MissionAndValuesClient } from "../MissionAndValues/MissionAndValuesClient";
 import { getMissionItemsFromDelivery } from "@/lib/missionApi";
